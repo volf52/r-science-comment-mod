926e137fea94d262abe1ffa4a386b5892667e0b1
Arslan <rslnkrmt2552@gmail.com>
2020-12-20 15:14:59 +0300

    Add Spacy BOW model
    - Test Experiment: https://ui.neptune.ai/volfy/testing/e/TES-17/charts
    - Final Training: https://ui.neptune.ai/volfy/testing/e/TES-18/charts

--- modcom/config.py
+++ modcom/config.py
@@ -10,16 +10,17 @@ from modcom.models import MLModel
 class AppConfig(BaseConfig):
     model_dir: Path = Path(__file__).parent.joinpath("../models").resolve()
     model_ext: str = "ml"
-
     vectorizer: str = "vectorizer"
 
     available_models: List[MLModel] = [
+        # MLModel(name="spacy_ensemble", type="spacy", display_name="Spacy - BOW + CNN"),
+        # MLModel(name="spacy_simple_cnn", type="spacy", display_name="Spacy - Simple CNN"),
+        MLModel(name="spacy_bow", type="spacy", display_name="Spacy - BOW"),
         MLModel(
             name="simple_logistic",
             type="sklearn",
             display_name="Simple Logistic Classifier",
         ),
-        MLModel(name="spacy_textcat", type="spacy", display_name="Spacy"),
     ]
 
 
